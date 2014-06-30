a0b4e6f86efb763bdc53aa5b3f2eb3b85634f621
Dare <dariush.zandi.n@gmail.com>
2014-06-30 09:18:37 +0430

    commit some style things from Eclips

--- src/ir/dariushzandi/android/crimnalintent/Crime.java
+++ src/ir/dariushzandi/android/crimnalintent/Crime.java
@@ -1,15 +1,17 @@
 package ir.dariushzandi.android.crimnalintent;
 
+import java.util.Date;
 import java.util.UUID;
-
-import android.R.string;
 
 public class Crime {
 	private UUID mId;
 	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
 	
 	public Crime(){
 		mId = UUID.randomUUID();
+		mDate = new Date();
 	}
 
 	public String getTitle() {
@@ -23,5 +25,23 @@ public class Crime {
 	public UUID getId() {
 		return mId;
 	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
+	}
+	
+	
 	
 }
